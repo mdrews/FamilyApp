5dccd528593a2e87d477b7d588cdc88c99d63c6c
mdrews <matthewdrews@gmail.com>
2019-10-07 01:24:46 -0400

    add item model

---
+++ models/Item.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ItemSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now()
+  }
+});
+
+const Item = mongoose('items', ItemSchema);
+
+module.exports = Item;
