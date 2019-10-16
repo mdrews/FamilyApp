ced4ceeb48bcdc683e468b2528d167b8c83919e7
mdrews <matthewdrews@gmail.com>
2019-10-16 00:14:33 -0400

    add redux store and container

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { connect, Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './App.css';
 import AppHeader from './components/AppHeader';
 import ShoppingList from './components/ShoppingList';
 import { addItem } from './actions/shoppingListActions';
+import { shoppingListReducer } from './reducers/shoppingListReducer';
 
 function App() {
   return (
@@ -26,5 +29,8 @@ const mapDispatchToProps = dispatch => {
     }
   }
 }
+
+const store = createStore(shoppingListReducer);
+const Container = connect(mapStateToProps, mapDispatchToProps)(App);
 
 export default App;
