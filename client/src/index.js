7a8f5243725d9b4022496e78c6652e00fddd5806
mdrews <matthewdrews@gmail.com>
2019-10-22 01:14:14 -0400

    fix redux devtools extension

--- client/src/index.js
+++ client/src/index.js
@@ -44,8 +44,8 @@ const store = createStore(
   shoppingListReducer, 
   initialState,
   compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(...middleware)
+    ,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
